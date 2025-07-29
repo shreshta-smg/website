c475437a8d6555163d1b7072a71c677ad1bf4049
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-07-29 16:02:50 +0530

    Refactoring for deploy,deps

--- skym-cms/config/middlewares.js
+++ skym-cms/config/middlewares.js
@@ -1,12 +1,42 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": ["'self'", "data:", "blob:"], // Add your CDN if using (e.g., Cloudinary)
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "https://skym-health.netlify.app",
+        "http://shreekrishnayogamandiram.in",
+        "http://localhost:3000",
+      ],
+      headers: "*",
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+    },
+  },
+  {
+    name: "strapi::poweredBy",
+    config: {
+      poweredBy: "Powered by shreshtasmg.in",
+    },
+  },
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];

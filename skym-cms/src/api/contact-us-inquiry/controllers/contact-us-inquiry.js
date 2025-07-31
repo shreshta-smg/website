f3fb5026cc65e7a7fa5370430ad7763c8123b7ae
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-07-31 09:27:27 +0530

    Refactoring v2.2

--- skym-cms/src/api/contact-us-inquiry/controllers/contact-us-inquiry.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * contact-us-inquiry controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::contact-us-inquiry.contact-us-inquiry');
--- skym-cms/src/api/contact-us-inquiry/services/contact-us-inquiry.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * contact-us-inquiry service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::contact-us-inquiry.contact-us-inquiry');
